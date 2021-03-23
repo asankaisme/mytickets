949e24cb4fe1d130f8fa8f920bc778084218098b
asankaisme <asankaisme@gmail.com>
2021-03-23 14:48:03 +0530

    other reports pending

--- resources/views/reports/repControl.blade.php
+++ resources/views/reports/repControl.blade.php
@@ -14,8 +14,32 @@
     <div class="border mb-3 pt-1 px-2 text-white rounded-top" style="background-color: rgba(11, 143, 22, 0.705)">
         <p>Print relavent report</p>
     </div>
-
-    <a href="{{ route('printStatus') }}" class="btn btn-dark" target="_blank">Status</a>
+    <div class="row">
+        <div class="col-sm-2">
+            Current Status Report :
+        </div>
+        <div class="col-sm-2">
+            <a href="{{ route('printStatus') }}" class="btn btn-outline-info btn-sm" target="_blank">Print</a>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-2">
+            Current Summary Report :
+        </div>
+        <div class="col-sm-2">
+            <a href="#" class="btn btn-outline-info btn-sm" target="_blank">Print</a>
+        </div>
+    </div>
+    <hr>
+    <div class="row">
+        <div class="col-sm-2">
+            Incidents reolved by SSEs (monthly) Report :
+        </div>
+        <div class="col-sm-2">
+            <a href="#" class="btn btn-outline-info btn-sm" target="_blank">Print</a>
+        </div>
+    </div>
 </div>
     
 @endsection
---
+++ resources/views/reports/sumRp.blade.php
@@ -0,0 +1,5 @@
+@extends('reports._RepLayout')
+
+@section('content')
+
+@endsection
